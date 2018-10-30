4875ce9b994c72d94c03533f57f7a6bf8cabc7e4
meetsav <31966195+meetsav@users.noreply.github.com>
2018-10-29 22:48:20 -0700

    Updated

--- edu/colostate/cs/cs414/StringCheese/src/Rook.java
+++ edu/colostate/cs/cs414/StringCheese/src/Rook.java
@@ -13,10 +13,309 @@ public class Rook extends ChessPiece {
 
     public HashSet<String> legalMoves(){
         String position = getPosition();
-
-
-
-        return legalMoves;
+        
+        HashSet<String> nextmove=new HashSet<String>();
+        HashSet<String> sidemove=new HashSet<String>();
+        HashSet<String> prevmove=new HashSet<String>();
+        
+        
+        
+        if(board.outerRing.contains(position))
+        {
+            int index=board.outerRing.indexOf(position);
+             try {
+                    prevmove=getPrevBackward(position);
+                    if(prevmove.isEmpty())
+                        legalMoves.addAll(prevmove);
+                } catch (IllegalPositionException ex) {
+                    Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            String nxtpos=position;
+            String sidepos=position;
+            if(index>=0 && index<6 )
+            {
+                
+                
+                if(index==5)
+                {
+                    int j=0;
+                    while(j!=6)
+                    {
+                        try {
+                            sidemove=getSideways(sidepos);
+                            if(sidemove.isEmpty())break;
+                             Iterator itr=sidemove.iterator();
+                             sidepos=itr.next().toString();
+                              legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        j++;
+                       
+                    }
+                }
+                else
+                {
+                    try {
+                            sidemove=getSideways(position);
+                            if(!sidemove.isEmpty())
+                                legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                }
+                int i=index;
+                while(i<13)
+                {
+                    try {
+                         nextmove=getNextForward(nxtpos);
+                         if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger. getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    i++;
+                }
+                
+            }
+            if(index>=6 && index<12)
+            {
+                 
+                 if(index==11)
+                 {
+                    int j=0;
+                    while(j!=6)
+                    {
+                        try {
+                            sidemove=getSideways(sidepos);
+                            if(sidemove.isEmpty())break;
+                             Iterator itr=sidemove.iterator();
+                             sidepos=itr.next().toString();
+                              legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        j++;
+                       
+                    }
+                }
+                 else
+                {
+                    try {
+                            sidemove=getSideways(position);
+                            if(!sidemove.isEmpty())
+                                legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                }
+                int i=index;
+                while(i<19)
+                {
+                    try {
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    i++;
+                }
+                
+            }
+            if(index>=12 && index<18)
+            {
+                
+                if(index==17)
+                {
+                    int j=0;
+                    while(j!=6)
+                    {
+                        try {
+                            sidemove=getSideways(sidepos);
+                            if(sidemove.isEmpty())break;
+                            Iterator itr=sidemove.iterator();
+                            sidepos=itr.next().toString();
+                            legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        j++;
+                       
+                    }
+                }
+                else
+                {
+                    try {
+                            sidemove=getSideways(position);
+                            if(!sidemove.isEmpty())
+                                legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                }
+                int i=index;
+                while(i<25)
+                {
+                    try {
+                        
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    i++;
+                }
+               
+            }
+            if(index>=18 && index<24)
+            {
+                
+                if(index==23)
+                {
+                    int j=0;
+                    while(j!=6)
+                    {
+                        try {
+                            sidemove=getSideways(sidepos);
+                            if(sidemove.isEmpty())break;
+                            Iterator itr=sidemove.iterator();
+                            sidepos=itr.next().toString();
+                            legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        j++;
+                       
+                    }
+                }
+                else
+                {
+                    try {
+                            sidemove=getSideways(position);
+                            
+                           if(!sidemove.isEmpty())
+                                legalMoves.addAll(sidemove);
+                        } catch (IllegalPositionException ex) {
+                            Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                }
+                int i=index;
+                while(i<31)
+                {
+                    try {
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    i++;
+                }
+                
+            }
+           
+           return legalMoves;
+        }
+        else if(board.innerRing.contains(position))
+        {
+            String nxtpos=position;
+            String sidepos=position;
+            int index=board.innerRing.indexOf(position);
+             try {
+                    prevmove=getPrevBackward(position);
+                    if(!prevmove.isEmpty())
+                        legalMoves.addAll(prevmove);
+                    
+                    sidemove=getSideways(position);
+                    if(!sidemove.isEmpty())
+                       legalMoves.addAll(sidemove);
+                } catch (IllegalPositionException ex) {
+                    Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            if(index>=0 && index<4)
+            {
+                int j=0;
+                while(j<=4)
+                {
+                    try {
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+            }
+            if(index>=4 && index<8)
+            {
+                int j=0;
+                while(j<=4)
+                {
+                    try {
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            if(index>=8 && index<11)
+            {
+                int j=0;
+                while(j<=4)
+                {
+                    try {
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+            }
+            if(index>=11 && index<15)
+            {
+                int j=0;
+                while(j<=4)
+                {
+                    try {
+                        nextmove=getNextForward(nxtpos);
+                        if(nextmove.isEmpty())break;
+                        Iterator itr=nextmove.iterator();
+                        nxtpos=itr.next().toString();
+                        legalMoves.addAll(nextmove);
+                    } catch (IllegalPositionException ex) {
+                        Logger.getLogger(Rook.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            return legalMoves;
+        }
+       else
+            return null;
+        
+       
+        
     }
 
     public String toString(){
