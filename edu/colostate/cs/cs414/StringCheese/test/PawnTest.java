ea8a056036668a90d4a5e6e68e9d849a8d453dd6
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2018-10-31 18:01:24 -0600

    PawnTest Update

--- edu/colostate/cs/cs414/StringCheese/test/PawnTest.java
+++ edu/colostate/cs/cs414/StringCheese/test/PawnTest.java
@@ -1,13 +1,56 @@
 package edu.colostate.cs.cs414.StringCheese.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.colostate.cs.cs414.StringCheese.src.ChessBoard;
+import edu.colostate.cs.cs414.StringCheese.src.ChessPiece;
+import edu.colostate.cs.cs414.StringCheese.src.IllegalPositionException;
+import edu.colostate.cs.cs414.StringCheese.src.Pawn;
+
 class PawnTest extends ChessSuite {
 
-    @org.junit.jupiter.api.Test
-    void legalMoves() {
-    }
+	ChessBoard board;
+	
+	@BeforeEach
+	void setUp() {
+		board = new ChessBoard();
+	}
 
-    @org.junit.jupiter.api.Test
-    public String toString() {
-        return null;
-    }
+	@Test
+	public void test() {
+		Pawn pawn = new Pawn(board, ChessPiece.Color.White);
+		pawn.setPosition("a1");
+
+		String expected = "a1";
+
+		assertEquals(pawn.getPosition(), expected);
+	}
+
+	@Test
+	public void testIllegalMove()  {
+		Pawn pawn = new Pawn(board, ChessPiece.Color.White);
+		pawn.setPosition("a1");
+
+		HashSet<String> expected = new HashSet<>();
+		expected.add("a2");
+		expected.add("b2");
+		HashSet<String> legalMoves = pawn.legalMoves();
+
+		assertEquals(legalMoves, expected);
+
+	}
+
+	/*
+	 * @org.junit.jupiter.api.Test void legalMoves() { }
+	 */
+
+	@org.junit.jupiter.api.Test
+	public String toString() {
+		return null;
+	}
 }
