d92c12acf77a6ae012af0df976e6a6c9d5a3c82c
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2018-11-25 11:18:42 -0700

    Update edu/colostate/cs/cs414/StringCheese/test/BishopTest.java

--- edu/colostate/cs/cs414/StringCheese/test/BishopTest.java
+++ edu/colostate/cs/cs414/StringCheese/test/BishopTest.java
@@ -1,13 +1,51 @@
 package edu.colostate.cs.cs414.StringCheese.test;
 
-class BishopTest extends ChessSuite {
+import edu.colostate.cs.cs414.StringCheese.src.ChessBoard;
+import edu.colostate.cs.cs414.StringCheese.src.ChessPiece;
+import edu.colostate.cs.cs414.StringCheese.src.Bishop;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BishopTest extends ChessSuite {
+    ChessBoard board = new ChessBoard();
+    //ChessPiece bishop = new Bishop(board, ChessPiece.Color.White);
+    
     @org.junit.jupiter.api.Test
     void legalMoves() {
+        Bishop bishop=new Bishop(board,ChessPiece.Color.White);
+        bishop.setPosition("g4");
+        board.placePiece(bishop, "g4");
+        HashSet<String> move= new HashSet<>();
+        move=bishop.legalMoves();
+        String expected="f3";
+        
+        assertTrue(move.contains(expected));
+        assertTrue(move.contains("e2"));
+        assertTrue(move.contains("d1"));
+        assertTrue(move.contains("c2"));
+        assertTrue(move.contains("b3"));
+        assertTrue(move.contains("a4"));
+        System.out.println("bishop:"+bishop.getPosition());
+        System.out.println("bishop:"+bishop.legalMoves());
+        
+        
     }
-
+    
     @org.junit.jupiter.api.Test
-    public String toString() {
-        return null;
+    void testBackward() {
+        Bishop bishop=new Bishop(board,ChessPiece.Color.White);
+        bishop.setPosition("g4");
+        board.placePiece(bishop, "g4");
+        HashSet<String> move= new HashSet<>();
+        move=bishop.legalMoves();
+        
+        System.out.println("bishop:"+bishop.getPosition());
+        assertTrue(move.contains("f5"));
+        System.out.println("bishop:"+bishop.legalMoves());
     }
 }
