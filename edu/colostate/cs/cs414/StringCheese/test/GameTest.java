f9b6b44fd1fcb3608abede77f29801bd9baf73d3
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2018-12-01 17:54:46 -0700

    Create GameTest.java

---
+++ edu/colostate/cs/cs414/StringCheese/test/GameTest.java
@@ -0,0 +1,114 @@
+package edu.colostate.cs.cs414.StringCheese.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGameUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGameIntStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testJoinGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testQuitGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEndTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEndTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetGameID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetGameID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInvitee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateDBGameState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUpdatedGameState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckGameStateUpdated() {
+		fail("Not yet implemented");
+	}
+
+}
