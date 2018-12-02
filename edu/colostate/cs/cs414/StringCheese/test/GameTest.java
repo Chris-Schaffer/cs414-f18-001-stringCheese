1c8c77ce0cb31803e6c95e62b9b182978e357cbc
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2018-12-02 16:45:26 -0700

    Update GameTest.java

--- edu/colostate/cs/cs414/StringCheese/test/GameTest.java
+++ edu/colostate/cs/cs414/StringCheese/test/GameTest.java
@@ -1,97 +1,117 @@
 package edu.colostate.cs.cs414.StringCheese.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
+import edu.colostate.cs.cs414.StringCheese.src.Game;
+import edu.colostate.cs.cs414.StringCheese.src.User;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class GameTest {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void testGameUser() {
-	}
-
-	@Test
-	void testGameIntStringStringStringStringString() {
-	}
-
-	@Test
-	void testJoinGame() {
-	}
-
-	@Test
-	void testQuitGame() {
-		
-	}
-
-	@Test
-	void testCreateGame() {
-		
-	}
-
-	@Test
-	void testGetStartTime() {
-		
-	}
-
-	@Test
-	void testGetEndTime() {
-		
-	}
-
-	@Test
-	void testSetEndTime() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetGameID() {
- 	}
-
-	@Test
-	void testSetGameID() {
-	}
-
-	@Test
-	void testGetHost() {
-	}
-
-	@Test
-	void testGetInvitee() {
-	}
-
-	@Test
-	void testGetResult() {
-	}
-
-	@Test
-	void testGetBoard() {
-	}
-
-	@Test
-	void testUpdateDBGameState() {
-		
-	}
-
-	@Test
-	void testGetUpdatedGameState() {
-		
-	}
-
-	@Test
-	void testCheckGameStateUpdated() {
-		
-	}
-
+    User user;
+    int gameID = 20;
+    Game game;
+    
+    @BeforeEach
+    void setUp() throws Exception {
+        String host = "john";
+        String invite = "chris";
+        String startTime = LocalDate.now().toString();
+        
+        ArrayList<Game> games = new ArrayList<>();
+        user = new User("joey123", "joey123@yahoo.com");
+        game = new Game(user);
+    }
+    
+    @AfterEach
+    void tearDown() throws Exception {
+        
+    }
+    
+    @Test
+    void testGameUser() {
+        String expectedName = "joey123";
+        String expectedEmail = "joey123@yahoo.com";
+        
+        assertEquals(true, expectedName == user.getName() && expectedEmail == user.getEmail());
+    }
+    
+    @Test
+    void testGameIntStringStringStringStringString() {
+    }
+    
+    @Test
+    void testJoinGame() {
+        boolean result = game.joinGame(gameID);
+        assertEquals(true, result);
+    }
+    
+    @Test
+    void testQuitGame() {
+        
+    }
+    
+    @Test
+    void testCreateGame() {
+        
+    }
+    
+    @Test
+    void testGetStartTime() {
+        
+    }
+    
+    @Test
+    void testGetEndTime() {
+        
+    }
+    
+    @Test
+    void testSetEndTime() {
+        //fail("Not yet implemented");
+    }
+    
+    @Test
+    void testGetGameID() {
+    }
+    
+    @Test
+    void testSetGameID() {
+    }
+    
+    @Test
+    void testGetHost() {
+    }
+    
+    @Test
+    void testGetInvitee() {
+    }
+    
+    @Test
+    void testGetResult() {
+    }
+    
+    @Test
+    void testGetBoard() {
+    }
+    
+    @Test
+    void testUpdateDBGameState() {
+        
+    }
+    
+    @Test
+    void testGetUpdatedGameState() {
+        
+    }
+    
+    @Test
+    void testCheckGameStateUpdated() {
+        
+    }
+    
 }
