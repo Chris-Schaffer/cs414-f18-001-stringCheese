8806d788e9fdaa207b6a85c03b19d3d8d90a0148
meetsav <Mks@12345>
2018-12-04 01:13:57 -0800

    Delete row instead of set is_active to 0

---
+++ edu/colostate/cs/cs414/StringCheese/test/InvitationTest.java
@@ -0,0 +1,12 @@
+package edu.colostate.cs.cs414.StringCheese.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvitationTest {
+
+    @Test
+    void sendInvitation() {
+    }
+}
